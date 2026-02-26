b2409f4db39b8d86c86eab81a9faca24f9208bbb
Marcus <marcus@local>
2026-02-26 10:05:52 -0700

    Set DEBUG to False for production readiness

--- mit_dashboard/settings.py
+++ mit_dashboard/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(ezn6$@@*9mvi*uy@we860d-=@ib8u4v9igtuav*h=vv-f!ndo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
