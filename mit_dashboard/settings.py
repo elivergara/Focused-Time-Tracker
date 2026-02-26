35bbdb4fbb99ca046063f745abb9e09d45b25223
Marcus <marcus@local>
2026-02-26 14:40:28 -0700

    Update STATIC_URL and refactor STATICFILES_DIRS for improved static file handling

--- mit_dashboard/settings.py
+++ mit_dashboard/settings.py
@@ -122,10 +122,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 #STATIC_URL = 'static/'
-STATIC_URL = 'static/'
-STATIC_ROOT = '/var/www/ftt/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-
+STATIC_URL = '/static/'
+#STATIC_ROOT = '/var/www/ftt/static/'
+# STATICFILES_DIRS = [BASE_DIR / 'static']
+import os
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 
 LOGIN_URL = '/accounts/login/'
